5d1ad0b63095b705609c0714613b30cbb9acfeb2
Jessy <you@example.com>
2024-11-20 14:52:15 +0100

    first commit

---
+++ src/test/java/com/group/teona/TeonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.teona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

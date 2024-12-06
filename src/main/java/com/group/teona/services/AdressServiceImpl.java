e786f8772fcdb114e537b5548fe4e5441bea7986
MalPro <malshis@yahoo.com>
2024-12-06 15:02:37 +0100

    purchase teonapass on filling the form

--- src/main/java/com/group/teona/services/AdressServiceImpl.java
+++ src/main/java/com/group/teona/services/AdressServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.group.teona.dto.FormTeonaPass;
 import com.group.teona.entities.Adress;
 import com.group.teona.entities.User;
 import com.group.teona.repositories.AdressRepository;
@@ -14,22 +15,38 @@ import com.group.teona.repositories.UserRepository;
 @Service
 public class AdressServiceImpl implements AdressService {
 	
-	@Autowired
-    UserRepository userRepository;
-	
-	@Autowired
-    AdressRepository adressRepository;
-	
-	@Override 
-	public Set<Adress> addAdresses (Set<Adress> adresses, User user) {
 
-			for (Adress adress : adresses) {
-				adress.setUser(user);
-			}
-			adressRepository.saveAll(adresses);
-			
-			return adresses;
-		
-	}
+    @Autowired
+    private AdressRepository adressRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveFormWithUser(FormTeonaPass formRequest, User user) {
+        Adress adress = mapToAdress(formRequest);
+        adress.setUser(user);
+        adressRepository.save(adress);
+    }
+
+    public void saveFormWithoutUser(FormTeonaPass formRequest) {
+        Adress adress = mapToAdress(formRequest);
+        adressRepository.save(adress);
+    }
+
+    private Adress mapToAdress(FormTeonaPass formRequest) {
+        Adress adress = new Adress();
+        adress.setStreetName(formRequest.getStreetName());
+        adress.setStreetNameOptional(formRequest.getStreetNameOptional());
+        adress.setPostCode(formRequest.getPostCode());
+        adress.setCity(formRequest.getCity());
+        adress.setCountry(formRequest.getCountry());
+        adress.setPhoneNumber(formRequest.getPhoneNumber());
+        adress.setFirstName(formRequest.getFirstName());
+        adress.setLastName(formRequest.getLastName());
+        adress.setImage(formRequest.getImage());
+        return adress;
+    }
+
+	
 
 }
--- src/main/java/com/group/teona/services/WalletServiceImpl.java
+++ src/main/java/com/group/teona/services/WalletServiceImpl.java
@@ -1,256 +1,256 @@
-package com.group.teona.services;
-
-
-import java.time.LocalDate;
-
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.group.teona.entities.Card;
-import com.group.teona.entities.Pass;
-import com.group.teona.entities.User;
-import com.group.teona.entities.Wallet;
-import com.group.teona.enums.EnumSub;
-import com.group.teona.repositories.CardRepository;
-import com.group.teona.repositories.PassRepository;
-import com.group.teona.repositories.UserRepository;
-import com.group.teona.repositories.WalletRepository;
-
-@Service
-public class WalletServiceImpl implements WalletService  {
-	
-	@Autowired
-    UserRepository userRepository;
-	
-	@Autowired
-    WalletRepository walletRepository;
-	
-	@Autowired
-    CardRepository cardRepository;
-	
-	@Autowired
-    PassRepository passRepository;
-
-	@Override
-	public Wallet addWallet(Wallet wallet, Card card, Pass pass, User user) {
-		
-
-		Optional<User> user = userRepository.findById(userId);
-		if(user.isPresent()) {
-			
-			User userFind = user.get();
-
-			wallet.setUser(userFind);
-		
-					if(card != null) {
-						card.setWallet(wallet);
-						
-						if(card.getAmount() > 0) {
-							card.setActive(true);
-						}
-
-						cardRepository.save(card);
-						
-					//	wallet.getCards().add(card);
-					}
-					
-					if(pass != null) {
-						pass.setWallet(wallet);
-						pass.setActive(true);
-						pass.setDateSubscription(LocalDate.now());
-									
-						// Si le pass est annuel
-						if(pass.getSubscriptionTime().equals(EnumSub.YearlyPass)) {
-										pass.setValideDuration(365.0);
-						}
-						
-
-						// Si le pass est mensuel
-						if(pass.getSubscriptionTime().equals(EnumSub.MounthlyPass)) {
-										pass.setValideDuration(30.0);
-						}
-						
-						// Si le pass est hebdomadaire
-						if(pass.getSubscriptionTime().equals(EnumSub.WeeklyPass)) {
-										pass.setValideDuration(7.0);
-						}
-						
-						// Si le pass est journalier
-						if(pass.getSubscriptionTime().equals(EnumSub.DayPass)) {
-										pass.setValideDuration(1.0);
-						}
-						
-						passRepository.save(pass);
-												
-
-					}
-					
-
-					return wallet;
-	
-	}
-	
-	@Override 
-	public Pass addNewPass (Pass pass,  User user) {
-		
-			Wallet walletUser = user.getWallet();
-			pass.setWallet(walletUser);
-			pass.setActive(true);
-			pass.setDateSubscription(LocalDate.now());
-						
-			// Si le pass est annuel
-			if(pass.getSubscriptionTime().equals(EnumSub.YearlyPass)) {
-							pass.setValideDuration(365.0);
-			}
-			
-			// Si le pass est mensuel
-			if(pass.getSubscriptionTime().equals(EnumSub.MounthlyPass)) {
-							pass.setValideDuration(30.0);
-			}
-			
-			// Si le pass est hebdomadaire
-			if(pass.getSubscriptionTime().equals(EnumSub.WeeklyPass)) {
-							pass.setValideDuration(7.0);
-			}
-			
-			// Si le pass est journalier
-			if(pass.getSubscriptionTime().equals(EnumSub.DayPass)) {
-							pass.setValideDuration(1.0);
-			}
-			
-			return passRepository.save(pass);
-		
-		
-	}
-	
-	@Override 
-	public Card addNewCard (User user) {
-		
-			Wallet walletUser = user.getWallet();
-			
-		
-			Card card = new Card();
-			card.setTopUp(0);
-			card.setWallet(walletUser);
-			card.setActive(false);
-			
-		
-			return cardRepository.save(card);
-		
-	}
-	
-	@Override
-	public Card add5TopUp (Long cardId) {
-		Optional<Card> card = cardRepository.findById(cardId);
-		Card cardFind = card.get();
-		
-		cardFind.setTopUp(5);
-		cardFind.setActive(true);
-		
-		return cardFind;
-	}
-	
-	@Override
-	public Card add10TopUp (Long cardId) {
-		Optional<Card> card = cardRepository.findById(cardId);
-		Card cardFind = card.get();
-		
-		cardFind.setTopUp(10);
-		cardFind.setActive(true);
-		
-		return cardFind;
-	}
-	
-	@Override
-	public Card add15TopUp (Long cardId) {
-		Optional<Card> card = cardRepository.findById(cardId);
-		Card cardFind = card.get();
-		
-		cardFind.setTopUp(15);
-		cardFind.setActive(true);
-		
-		return cardFind;
-	}
-	
-	@Override
-	public Card add20TopUp (Long cardId) {
-		Optional<Card> card = cardRepository.findById(cardId);
-		Card cardFind = card.get();
-		
-		cardFind.setTopUp(20);
-		cardFind.setActive(true);
-		
-		return cardFind;
-	}
-	
-	@Override
-	public Card addPersoTopUp (Long cardId, Long topUp) {
-		Optional<Card> card = cardRepository.findById(cardId);
-		Card cardFind = card.get();
-		
-		cardFind.setTopUp(topUp);
-		cardFind.setActive(true);
-		
-		return cardFind;
-	}
-	
-	
-	/*
-	@Override
-	public String useCard(Long cardId) {
-		Optional<Card> card = cardRepository.findById(cardId);
-		if (card.isPresent()) {
-			Card cardFind = card.get();
-					if(cardFind.isActive()) {
-						cardFind.setAmount(cardFind.getAmount() - 1);
-							if(cardFind.getAmount() == 0) {
-								cardFind.setActive(false);
-							}
-						return "Trajet validé, solde restant : " + cardFind.getAmount();
-					}
-					throw new RuntimeException("Solde épuisé, veuillez acheter une nouvelle carte");
-		}
-		throw new RuntimeException("Carte inexistante");
-		
-	}
-	*/
-
-	
-	@Override 
-	public Card addNewCard (Long userId, Card card) {
-		
-		Optional<User> user = userRepository.findById(userId);
-		if(user.isPresent()) {
-			
-			User userFind = user.get();
-			Wallet walletUser = userFind.getWallet();
-			// walletUser.getCards().add(card);
-			card.setWallet(walletUser);
-			
-			return cardRepository.save(card);
-		}
-		throw new RuntimeException("Utilisateur non trouvé");
-		
-	}
-	
-	
-	@Override
-	public String useCard(Long cardId) {
-		Optional<Card> card = cardRepository.findById(cardId);
-		if (card.isPresent()) {
-			Card cardFind = card.get();
-					if(cardFind.getAmount() > 0) {
-						cardFind.setAmount(cardFind.getAmount() - 1);
-						return "Trajet validé, solde restant : " + cardFind.getAmount();
-					}
-					throw new RuntimeException("Solde épuisé, veuillez acheter une nouvelle carte");
-		}
-		throw new RuntimeException("Carte inexistante");
-		
-	}
-
-	
-
-}
+//package com.group.teona.services;
+//
+//
+//import java.time.LocalDate;
+//
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.group.teona.entities.Card;
+//import com.group.teona.entities.Pass;
+//import com.group.teona.entities.User;
+//import com.group.teona.entities.Wallet;
+//import com.group.teona.enums.EnumSub;
+//import com.group.teona.repositories.CardRepository;
+//import com.group.teona.repositories.PassRepository;
+//import com.group.teona.repositories.UserRepository;
+//import com.group.teona.repositories.WalletRepository;
+//
+//@Service
+//public class WalletServiceImpl implements WalletService  {
+//	
+//	@Autowired
+//    UserRepository userRepository;
+//	
+//	@Autowired
+//    WalletRepository walletRepository;
+//	
+//	@Autowired
+//    CardRepository cardRepository;
+//	
+//	@Autowired
+//    PassRepository passRepository;
+//
+//	@Override
+//	public Wallet addWallet(Wallet wallet, Card card, Pass pass, User user) {
+//		
+//
+//		Optional<User> user = userRepository.findById(userId);
+//		if(user.isPresent()) {
+//			
+//			User userFind = user.get();
+//
+//			wallet.setUser(userFind);
+//		
+//					if(card != null) {
+//						card.setWallet(wallet);
+//						
+//						if(card.getAmount() > 0) {
+//							card.setActive(true);
+//						}
+//
+//						cardRepository.save(card);
+//						
+//					//	wallet.getCards().add(card);
+//					}
+//					
+//					if(pass != null) {
+//						pass.setWallet(wallet);
+//						pass.setActive(true);
+//						pass.setDateSubscription(LocalDate.now());
+//									
+//						// Si le pass est annuel
+//						if(pass.getSubscriptionTime().equals(EnumSub.YearlyPass)) {
+//										pass.setValideDuration(365.0);
+//						}
+//						
+//
+//						// Si le pass est mensuel
+//						if(pass.getSubscriptionTime().equals(EnumSub.MounthlyPass)) {
+//										pass.setValideDuration(30.0);
+//						}
+//						
+//						// Si le pass est hebdomadaire
+//						if(pass.getSubscriptionTime().equals(EnumSub.WeeklyPass)) {
+//										pass.setValideDuration(7.0);
+//						}
+//						
+//						// Si le pass est journalier
+//						if(pass.getSubscriptionTime().equals(EnumSub.DayPass)) {
+//										pass.setValideDuration(1.0);
+//						}
+//						
+//						passRepository.save(pass);
+//												
+//
+//					}
+//					
+//
+//					return wallet;
+//	
+//	}
+//	
+//	@Override 
+//	public Pass addNewPass (Pass pass,  User user) {
+//		
+//			Wallet walletUser = user.getWallet();
+//			pass.setWallet(walletUser);
+//			pass.setActive(true);
+//			pass.setDateSubscription(LocalDate.now());
+//						
+//			// Si le pass est annuel
+//			if(pass.getSubscriptionTime().equals(EnumSub.YearlyPass)) {
+//							pass.setValideDuration(365.0);
+//			}
+//			
+//			// Si le pass est mensuel
+//			if(pass.getSubscriptionTime().equals(EnumSub.MounthlyPass)) {
+//							pass.setValideDuration(30.0);
+//			}
+//			
+//			// Si le pass est hebdomadaire
+//			if(pass.getSubscriptionTime().equals(EnumSub.WeeklyPass)) {
+//							pass.setValideDuration(7.0);
+//			}
+//			
+//			// Si le pass est journalier
+//			if(pass.getSubscriptionTime().equals(EnumSub.DayPass)) {
+//							pass.setValideDuration(1.0);
+//			}
+//			
+//			return passRepository.save(pass);
+//		
+//		
+//	}
+//	
+//	@Override 
+//	public Card addNewCard (User user) {
+//		
+//			Wallet walletUser = user.getWallet();
+//			
+//		
+//			Card card = new Card();
+//			card.setTopUp(0);
+//			card.setWallet(walletUser);
+//			card.setActive(false);
+//			
+//		
+//			return cardRepository.save(card);
+//		
+//	}
+//	
+//	@Override
+//	public Card add5TopUp (Long cardId) {
+//		Optional<Card> card = cardRepository.findById(cardId);
+//		Card cardFind = card.get();
+//		
+//		cardFind.setTopUp(5);
+//		cardFind.setActive(true);
+//		
+//		return cardFind;
+//	}
+//	
+//	@Override
+//	public Card add10TopUp (Long cardId) {
+//		Optional<Card> card = cardRepository.findById(cardId);
+//		Card cardFind = card.get();
+//		
+//		cardFind.setTopUp(10);
+//		cardFind.setActive(true);
+//		
+//		return cardFind;
+//	}
+//	
+//	@Override
+//	public Card add15TopUp (Long cardId) {
+//		Optional<Card> card = cardRepository.findById(cardId);
+//		Card cardFind = card.get();
+//		
+//		cardFind.setTopUp(15);
+//		cardFind.setActive(true);
+//		
+//		return cardFind;
+//	}
+//	
+//	@Override
+//	public Card add20TopUp (Long cardId) {
+//		Optional<Card> card = cardRepository.findById(cardId);
+//		Card cardFind = card.get();
+//		
+//		cardFind.setTopUp(20);
+//		cardFind.setActive(true);
+//		
+//		return cardFind;
+//	}
+//	
+//	@Override
+//	public Card addPersoTopUp (Long cardId, Long topUp) {
+//		Optional<Card> card = cardRepository.findById(cardId);
+//		Card cardFind = card.get();
+//		
+//		cardFind.setTopUp(topUp);
+//		cardFind.setActive(true);
+//		
+//		return cardFind;
+//	}
+//	
+//	
+//	/*
+//	@Override
+//	public String useCard(Long cardId) {
+//		Optional<Card> card = cardRepository.findById(cardId);
+//		if (card.isPresent()) {
+//			Card cardFind = card.get();
+//					if(cardFind.isActive()) {
+//						cardFind.setAmount(cardFind.getAmount() - 1);
+//							if(cardFind.getAmount() == 0) {
+//								cardFind.setActive(false);
+//							}
+//						return "Trajet validé, solde restant : " + cardFind.getAmount();
+//					}
+//					throw new RuntimeException("Solde épuisé, veuillez acheter une nouvelle carte");
+//		}
+//		throw new RuntimeException("Carte inexistante");
+//		
+//	}
+//	*/
+//
+//	
+//	@Override 
+//	public Card addNewCard (Long userId, Card card) {
+//		
+//		Optional<User> user = userRepository.findById(userId);
+//		if(user.isPresent()) {
+//			
+//			User userFind = user.get();
+//			Wallet walletUser = userFind.getWallet();
+//			// walletUser.getCards().add(card);
+//			card.setWallet(walletUser);
+//			
+//			return cardRepository.save(card);
+//		}
+//		throw new RuntimeException("Utilisateur non trouvé");
+//		
+//	}
+//	
+//	
+//	@Override
+//	public String useCard(Long cardId) {
+//		Optional<Card> card = cardRepository.findById(cardId);
+//		if (card.isPresent()) {
+//			Card cardFind = card.get();
+//					if(cardFind.getAmount() > 0) {
+//						cardFind.setAmount(cardFind.getAmount() - 1);
+//						return "Trajet validé, solde restant : " + cardFind.getAmount();
+//					}
+//					throw new RuntimeException("Solde épuisé, veuillez acheter une nouvelle carte");
+//		}
+//		throw new RuntimeException("Carte inexistante");
+//		
+//	}
+//
+//	
+//
+//}
